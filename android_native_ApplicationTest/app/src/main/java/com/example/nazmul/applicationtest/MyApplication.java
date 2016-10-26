3aa81e97e29cd8a4e5bc9842824de333d0b74bb2
Nazmul Idris <nazmul@fasterlap.com>
2016-10-26 13:24:52 -0700

    1) native android app experimentation ..

---
+++ android_native_ApplicationTest/app/src/main/java/com/example/nazmul/applicationtest/MyApplication.java
@@ -0,0 +1,38 @@
+package com.example.nazmul.applicationtest;
+
+import android.app.Application;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Random;
+
+/**
+ * Created by nazmul on 10/26/16.
+ */
+
+public class MyApplication extends Application {
+private static final String TAG = MyApplication.class.getSimpleName();
+private HashMap<String, String> _state;
+
+@Override
+public void onCreate() {
+  super.onCreate();
+  Log.d(TAG, "onCreate: [START]");
+  _state = new HashMap<String, String>();
+  Log.d(TAG, "onCreate: created a new state object");
+  _state.put(String.valueOf(new Random().nextInt(1000)), "value");
+  String msg = _state.toString();
+  Log.d(TAG, String.format("onCreate: {%s} [END]", msg));
+}
+
+@Override
+public void onTerminate() {
+  super.onTerminate();
+  Log.d(TAG, "onTerminate: ran");
+}
+
+public HashMap<String, String> getState(){
+  return _state;
+}
+
+}
