4185568116d71f29a6d8191b687392995e712e02
Christopher Dunavan <github@hypersprite.com>
2016-10-28 11:09:39 -0700

    1) Added flowtypes.js file for interfaces 2) Moving to JSX files for components and containers

---
+++ web/src/global/flowtypes.js
@@ -0,0 +1,68 @@
+// @flow
+//
+// List of all Flow types used in the container and ui package.
+// These only apply to the files with '// flow' at the top.
+
+export type ReduxActionIF = {
+  type: string,
+  payload: any,
+}
+
+export type ChatMessageIF = {
+  message: string,
+  timestamp: number,
+  displayName: string,
+  photoURL: string,
+  sessionId: string,
+}
+
+export type ChatMeassageListItemIF = {
+  key: number,
+  index: number,
+  avatarIconSize: number,
+  chatMessage: ChatMessageIF,
+}
+
+
+/** this represents a user object */
+export type UserIF = {
+  displayName: string;
+  photoURL: string;
+  isAnonymous: boolean;
+  email: string;
+  emailVerified: boolean;
+  uid: string;
+  timestamp: any;
+  googleAccessToken?: any;
+}
+
+export type PresenceIF = {
+  status: string,
+  user: UserIF,
+}
+
+/** holds information about the user who is signed in */
+export type AuthStateIF = {
+  old_uid?: string;
+  new_uid?: string;
+  new_user?: UserIF;
+  old_user?: UserIF;
+}
+
+/** this represents a constituent element of the todoArray */
+export type TodoIF = {
+  item: string;
+  done: boolean;
+}
+
+/** this represents the data that is stored in firebase */
+export type DataIF = {
+  todoArray: TodoIF[];
+  sessionId?: any;
+  timestamp?: any;
+}
+
+export type ReduxStateIF = {
+  user: UserIF,
+  data: DataIF,
+}
