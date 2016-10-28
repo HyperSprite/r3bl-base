e88daca552d9a4e00704320e10d51c6ca3bb8aed
Nazmul Idris <nazmul@fasterlap.com>
2016-10-27 17:22:29 -0700

    1) minor refactoring ...

--- android_native_ApplicationTest/app/src/main/java/com/example/nazmul/applicationtest/MainActivity.java
+++ android_native_ApplicationTest/app/src/main/java/com/example/nazmul/applicationtest/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -23,18 +22,17 @@ protected void onCreate(Bundle savedInstanceState) {
   setSupportActionBar(toolbar);
 
   FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-  fab.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
+  fab.setOnClickListener(
+    view ->
       Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-              .setAction("Action", v ->{
-                MyApplication ctx = (MyApplication) getApplicationContext();
-                String msg = ctx.getState().toString();
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-              })
-              .show();
-    }
-  });
+              .setAction("Action",
+                         v -> {
+                           MyApplication ctx = (MyApplication) getApplicationContext();
+                           String msg = ctx.getState().toString();
+                           Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT)
+                                .show();
+                         })
+              .show());
 }
 
 @Override
