53ab467314a7864043f58e155629d7813532b89b
Christopher Dunavan <github@hypersprite.com>
2016-10-27 21:13:09 -0700

    1) converted everthing back to JS and made sure it works

--- src/client/main.js
+++ src/client/main.js
@@ -4,8 +4,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {App} from './ui/app';
+import { Provider } from 'react-redux';
+import { App } from './ui/app';
+import { applicationContext } from './container/context';
 
 /** Material UI requirement */
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -16,8 +17,6 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
  * more info - http://stackoverflow.com/a/34015469/988941
  */
 injectTapEventPlugin();
-
-import {applicationContext} from './container/context';
 
 ReactDOM.render(
   <Provider store={applicationContext.getReduxStore()}>
