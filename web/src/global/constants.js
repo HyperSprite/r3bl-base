a4b85ee3538939b71bfd8dde32c20132b687e744
Christopher Dunavan <github@hypersprite.com>
2016-11-10 10:18:14 -0800

    1) Added hypersprite firebase cred

--- web/src/global/constants.js
+++ web/src/global/constants.js
@@ -7,26 +7,27 @@
 
 /** these are event names */
 const GLOBAL_CONSTANTS = {
-  REMOTE_MESSAGE_FROM_CLIENT: "messageFromClient",
-  REMOTE_MESSAGE_FROM_SERVER: "messageFromServer",
-  LE_SHOW_SNACKBAR: "localevent_showSnackbar",
-  LE_SET_USER: "localevent_setUser",
-  LE_PRESENCE_USER_ADDED: "localevent_presenceUserAdded",
-  LE_PRESENCE_USER_REMOVED: "localevent_presenceUserRemoved",
-  LE_PRESENCE_USER_CHANGED: "localevent_presenceUserChanged",
-  LE_CONTAINER_NETWORK_CONNECTION_STATE: "localevent_containerNetworkConnectionState",
+  REMOTE_MESSAGE_FROM_CLIENT: 'messageFromClient',
+  REMOTE_MESSAGE_FROM_SERVER: 'messageFromServer',
+  LE_SHOW_SNACKBAR: 'localevent_showSnackbar',
+  LE_SET_USER: 'localevent_setUser',
+  LE_PRESENCE_USER_ADDED: 'localevent_presenceUserAdded',
+  LE_PRESENCE_USER_REMOVED: 'localevent_presenceUserRemoved',
+  LE_PRESENCE_USER_CHANGED: 'localevent_presenceUserChanged',
+  LE_CONTAINER_NETWORK_CONNECTION_STATE: 'localevent_containerNetworkConnectionState',
 };
 
 /** this determines if log messages are shown or not in the console */
 const LOGGING_ENABLED = true;
 
+// HyperSprite Firebase Keys
 /** put your firebase config data here */
 const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBRJ4xl-15s08fI_H82fX-weap0oHjOcwc",
-  authDomain: "todolist-starter-project.firebaseapp.com",
-  databaseURL: "https://todolist-starter-project.firebaseio.com",
-  storageBucket: "todolist-starter-project.appspot.com",
-  messagingSenderId: "455882594322"
+  apiKey: 'AIzaSyB-DPgWoc_XoQGnmHp96w6JfKjtwLrK6Xo',
+  authDomain: 'todolist-react-redux.firebaseapp.com',
+  databaseURL: 'https://todolist-react-redux.firebaseio.com',
+  storageBucket: 'todolist-react-redux.appspot.com',
+  messagingSenderId: '482928248882',
 };
 
 /** export these constants */
